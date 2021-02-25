7973d884b39446939a5259afa4562c9332897be2
feelingBlue00 <pta230500x090800@gmail.com>
2021-02-26 00:03:07 +0700

    Convert keys list to dictionary and initalize values with None

--- extractValue.py
+++ extractValue.py
@@ -1,11 +1,5 @@
-import json 
+import json
 
-def extract_keys(obj, keys_list):
-    keys_list += obj.keys()
-    for key, value in obj.items():
-        if isinstance(value, dict):
-            extract_keys(value, keys_list)
-        
 def extract_values(obj, value_list):
     if (not isinstance(obj.values(), dict) and not isinstance(obj.values(), list)):
         value_list += obj.values()
@@ -13,16 +7,8 @@ def extract_values(obj, value_list):
         if isinstance(value, dict):
             extract_values(value, value_list)
 
-
 j_file = open("test.json")
 json_data = json.load(j_file)
 
-keys = []
-extract_keys(json_data, keys)
-
 values = []
 extract_values(json_data, values)
-
-#print(keys)
-print(values)
-
