557baaa104931b6e9c6ac1521080064c898927f8
feelingBlue00 <pta230500x090800@gmail.com>
2021-02-25 11:49:24 +0700

    Modified

--- extract.py
+++ extract.py
@@ -6,19 +6,23 @@ def extract_keys(obj, keys_list):
         if isinstance(value, dict):
             extract_keys(value, keys_list)
         
+def extract_values(obj, value_list):
+    if (not isinstance(obj.values(), dict) and not isinstance(obj.values(), list)):
+        value_list += obj.values()
+    for key, value in obj.items():
+        if isinstance(value, dict):
+            extract_values(value, value_list)
+
 
 j_file = open("test.json")
 json_data = json.load(j_file)
 
 keys = []
 extract_keys(json_data, keys)
-print(keys)
-#print(len(keys))
 
+values = []
+extract_values(json_data, values)
 
-
-        
-
-
-
+#print(keys)
+print(values)
 
