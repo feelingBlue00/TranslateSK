f83d20aa31d672432504c4879b82ef04319a491a
feelingBlue00 <pta230500x090800@gmail.com>
2021-02-26 09:10:05 +0700

    Add json file for keys

--- extractKeys.py
+++ extractKeys.py
@@ -6,11 +6,14 @@ def extract_keys(obj, keys_list):
         if isinstance(value, dict):
             extract_keys(value, keys_list)
         
-j_file = open("test.json")
+j_file = open("en-GB.json")
 json_data = json.load(j_file)
 
 keys = []
 extract_keys(json_data, keys)
-print({key: None for key in keys})
+#print({key: None for key in keys})
+
+with open("keys.json", "w") as json_keys:
+    json.dump({key: None for key in keys}, json_keys)
 
 
--- translate.py
+++ translate.py
@@ -1,7 +1,7 @@
 from googletrans import Translator
 
 translator = Translator()
-translation = translator.translate('veritas lux mea', src='la')
 
-print(translation.text)
+translations = {}
+
 
