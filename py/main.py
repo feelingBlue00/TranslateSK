8ff46335cc81fe05d5bce7f9f85f34c6fcbc1613
feelingBlue00 <pta230500x090800@gmail.com>
2021-03-02 14:53:15 +0700

    Concate keys

--- py/main.py
+++ py/main.py
@@ -1,1 +1,5 @@
 from readCSV import readCSV
+
+readCSV()
+
+
--- py/readCSV.py
+++ py/readCSV.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 def readCSV():
     df = pd.read_csv("../TranslateSheet1 - Copy.csv")
-    matrix2 = df[df.columns[0]].to_numpy()
-    csv_key = matrix2.tolist()
-#    print(csv_key)
+    keys = df[df.columns[0]].to_numpy()
+    vi = df[df.column[2]].to_numpy()
+    csv_key = keys.tolist()
+    csv_vi = vi.tolist()
+    print(csv_key + ": " + csv_vi)
